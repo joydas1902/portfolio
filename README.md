10cb7ec673d7c31b7c5c3fbcbd128b8971c88a7a
JoyDas1902 <ijoydas1902@gmail.com>
2024-02-11 23:26:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Portfolio App
+
+A portfolio web app using Next.js, seamlessly blending creativity and professionalism. Enhanced user interactions with Framer Motion animations and maintained clean, modular styles using Tailwind CSS. Elevating my online presence with a modern and visually appealing showcase of skills and projects.
+
+First, install the dependencies:
+
+```bash
+npm install
+# or
+yarn install
+# or
+pnpm install
+# or
+bun install
+```
+
+run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ src/app/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const Homepage = () => {
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row lg:gap-8 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image src="/hero.jpg" alt="" fill className="object-contain" />
+        </div>
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Turning Vision Into Reality With Code And Design.
+          </h1>
+          <p className="md:text-xl">
+            Welcome to my digital canvas, where innovation and creativity
+            converge. With a keen eye for aesthetics and a mastery of code, my
+            portfolio showcases a diverse collection of projects that reflect my
+            commitment to excellence.
+          </p>
+          <div className="w-full flex gap-4">
+            <Link href='/portfolio' className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Work
+            </Link>
+            <Link href='/contact' className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </Link>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Homepage;
